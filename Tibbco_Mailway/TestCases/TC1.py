8bf64f09e74b2cbb73886f75b43a8c1b71da70dc
Rohit Kumar <your-email@example.com>
2026-06-25 13:00:53 +0530

    Deleted TestCases Files

--- Tibbco_Mailway/TestCases/TC1.py
+++
@@ -1,3 +0,0 @@
-def test_case_1(my_client, Username, Password):
-    # Test case 1 implementation
-    pass
--- Tibbco_Mailway/TestCases/TC2.py
+++
@@ -1,3 +0,0 @@
-def test_case_2(my_client, Username, Password):
-    # Test case 2 implementation
-    pass
